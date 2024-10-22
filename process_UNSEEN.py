45cbdc4abf893955bd98ba3536c0cb9a706dcff5
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2024-10-22 10:20:21 +0100

    testing SDM

--- process_UNSEEN.py
+++ process_UNSEEN.py
@@ -879,20 +879,20 @@ def main():
     print(model_df_ondjfm.head())
 
     # # plot the fidelity testing
-    # funcs.plot_fidelity(
-    #     obs_df=obs_df,
-    #     model_df=model_df_ondjfm,
-    #     obs_val_name=obs_val_name,
-    #     model_val_name=model_val_name,
-    #     obs_time_name="time",
-    #     model_time_name="init_year",
-    #     model_member_name="member",
-    #     model_lead_name="lead",
-    #     nboot=1000, # 1000 bootstraps for testing
-    #     figsize=(10, 8),
-    #     save_dir=save_dir,
-    #     fname_root=f"fidelity_{args.variable}_{args.country}_{args.season}_{args.first_year}_{args.last_year}_{model}_{experiment}_{freq}_fcst_year_{args.model_fcst_year}_lead_year_{args.lead_year}_obs-{obs_val_name}_model-{model_val_name}_bc-{args.bias_correct}",
-    # )
+    funcs.plot_fidelity(
+        obs_df=obs_df,
+        model_df=model_df_ondjfm,
+        obs_val_name=obs_val_name,
+        model_val_name=model_val_name,
+        obs_time_name="time",
+        model_time_name="init_year",
+        model_member_name="member",
+        model_lead_name="lead",
+        nboot=1000, # 1000 bootstraps for testing
+        figsize=(10, 8),
+        save_dir=save_dir,
+        fname_root=f"fidelity_{args.variable}_{args.country}_{args.season}_{args.first_year}_{args.last_year}_{model}_{experiment}_{freq}_fcst_year_{args.model_fcst_year}_lead_year_{args.lead_year}_obs-{obs_val_name}_model-{model_val_name}_bc-{args.bias_correct}",
+    )
 
 
     # Plot the return period
@@ -906,12 +906,6 @@ def main():
         save_prefix=f"return_period_{args.variable}_{args.country}_{args.season}_{args.first_year}_{args.last_year}_{model}_{experiment}_{freq}_fcst_year_{args.model_fcst_year}_lead_year_{args.lead_year}_obs-{obs_val_name}_model-{model_val_name}_bc-{args.bias_correct}",
         save_dir=save_dir,
     )
-
-
-    # print how long the script took
-    print(f"Script took {time.time() - start} seconds")
-    print("----------------")
-    print("Script complete")
 
     # plot the extreme events for the given variable
     funcs.plot_events_ts(
@@ -955,6 +949,11 @@ def main():
     #         fig_size=(6, 6),
     #         fname_root=f"stability_boxplots_{args.variable}_{args.country}_{args.season}_{args.first_year}_{args.last_year}_{model}_{experiment}_{freq}_fcst_year_{args.model_fcst_year}_lead_year_{args.lead_year}_model-{model_val_name}_bc-{args.bias_correct}",
     #     )
+
+    # print how long the script took
+    print(f"Script took {time.time() - start} seconds")
+    print("----------------")
+    print("Script complete")
 
 # Run the main function
 if __name__ == "__main__":
