ac410d278117d4e4cffaddda13c7aa967d091f8a
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2024-11-04 18:37:17 +0000

    updated for running cesm and cmcc

--- submit_process_unseen.bash
+++ submit_process_unseen.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=sub-proc-unseen
 #SBATCH --partition=high-mem
-#SBATCH --mem=400000
+#SBATCH --mem=800000
 #SBATCH --time=1000:00
 #SBATCH -o /home/users/benhutch/unseen_functions/logs/sub-proc-unseen-%A_%a.out
 #SBATCH -e /home/users/benhutch/unseen_functions/logs/sub-proc-unseen-%A_%a.err
